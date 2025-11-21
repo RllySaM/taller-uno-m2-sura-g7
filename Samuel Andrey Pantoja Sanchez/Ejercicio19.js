e8b4f3de3b53d6d858e60a3676d5331cfe1db3d8
RllySaM <andreypantoja001@gmail.com>
2025-11-21 11:44:39 -0500

    Reenviado Ej19

---
+++ Samuel Andrey Pantoja Sanchez/Ejercicio19.js
@@ -0,0 +1,7 @@
+let tablero = ["Inicio", null, null, null, null, "Fin"];
+
+console.log("Tablero inicial:", tablero);
+
+// 1
+tablero.fill("Casilla Vacía", 1, 5);
+console.log("Después de rellenar casillas nulas:", tablero);
