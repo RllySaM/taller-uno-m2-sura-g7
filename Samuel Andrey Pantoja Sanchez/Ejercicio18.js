9eb63a19a689e58d5f0fad525d7c3de1f68b18fb
RllySaM <andreypantoja001@gmail.com>
2025-11-21 11:44:01 -0500

    Reenviado Ej18

--- Samuel Andrey Pantoja Sanchez/Ejercicio18.js
+++ Samuel Andrey Pantoja Sanchez/Ejercicio18.js
@@ -1,1 +1,11 @@
-console.log("Hola Mundo");
+let invitados = ["Carlos", "Beatriz", "David", "Ana"];
+
+console.log("Lista inicial de invitados:", invitados);
+
+// 1
+invitados.sort();
+console.log("Lista ordenada alfabéticamente:", invitados);
+
+// 2
+invitados.splice(1, 1);
+console.log("Después de que Beatriz cancela:", invitados);
